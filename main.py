cb4894f7e8c55f73ef75aeca02cb45083918714c
JPCODESZ <jonpeppy2000@gmail.com>
2025-03-27 21:45:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException
 import requests
 import os
 from dotenv import load_dotenv
